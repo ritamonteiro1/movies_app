350f84b58988eefa3221cb281f977de3059fe6ec
Alvaro Prado <alvarob.prado@hotmail.com>
2023-02-14 23:00:25 -0400

    feat: create PopularMoviesActivity

--- featurepopularmovies/build.gradle.kts
+++ featurepopularmovies/build.gradle.kts
@@ -3,6 +3,7 @@ plugins {
     id(BuildPlugins.KOTLIN_ANDROID)
     id(BuildPlugins.KOTLIN_PARCELIZE)
     id(BuildPlugins.KOTLIN_KAPT)
+    id("org.jetbrains.kotlin.android")
 }
 
 android {
@@ -57,6 +58,9 @@ android {
 }
 
 dependencies {
+    implementation("androidx.appcompat:appcompat:1.6.0")
+    implementation("com.google.android.material:material:1.4.0")
+    implementation("androidx.constraintlayout:constraintlayout:2.1.4")
     androidx()
     implementation(Libs.MATERIAL_DESIGN)
     implementation(Libs.CONSTRAINT_LAYOUT)
---
+++ featurepopularmovies/src/main/java/com/example/featurepopularmovies/PopularMoviesActivity.kt
@@ -0,0 +1,11 @@
+package com.example.featurepopularmovies
+
+import android.os.Bundle
+import androidx.appcompat.app.AppCompatActivity
+
+class PopularMoviesActivity : AppCompatActivity() {
+    override fun onCreate(savedInstanceState: Bundle?) {
+        super.onCreate(savedInstanceState)
+        setContentView(R.layout.activity_popular_movies)
+    }
+}
