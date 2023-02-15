dab68cabfd13173e611859c239f2dc21740081ea
Alvaro Prado <alvarob.prado@hotmail.com>
2023-02-14 21:56:02 -0400

    feat: implements SimilarMoviesRepository

--- data/src/main/java/com/example/data/repository/SimilarMoviesRepositoryImpl.kt
+++ data/src/main/java/com/example/data/repository/SimilarMoviesRepositoryImpl.kt
@@ -1,7 +1,18 @@
 package com.example.data.repository
 
+import com.example.core.utils.Result
+import com.example.data.mappers.toSimilarDomain
+import com.example.data.remote.datasource.SimilarMoviesRemoteDataSource
+import com.example.featuresimilarmovies.domain.models.SimilarMovieList
 import com.example.featuresimilarmovies.domain.repository.SimilarMoviesRepository
 
-class SimilarMoviesRepositoryImpl : SimilarMoviesRepository {
-
+class SimilarMoviesRepositoryImpl(
+    private val remoteDataSource: SimilarMoviesRemoteDataSource
+) : SimilarMoviesRepository {
+    override suspend fun getSimilarMovies(movieId: Int): Result<SimilarMovieList> {
+        return when (val result = remoteDataSource.getSimilarMovies(movieId)) {
+            is Result.Success -> Result.Success(result.data.toSimilarDomain())
+            is Result.Error -> Result.Error(result.error)
+        }
+    }
 }
