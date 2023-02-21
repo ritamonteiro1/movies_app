0fae26e43eb26f9b4a4d7e37645a381edffd3665
ritamonteiro1 <ritaalvesmonteiro@gmail.com>
2023-02-20 21:39:57 -0300

    create object Modules

---
+++ buildSrc/src/main/java/Modules.kt
@@ -0,0 +1,10 @@
+object Modules {
+    const val CORE = ":core"
+    const val DATA = ":data"
+    const val DESIGN_SYSTEM = ":designsystem"
+    const val DOMAIN = ":domain"
+    const val DI = ":di"
+    const val FEATURE_POPULAR_MOVIES = ":featurepopularmovies"
+    const val FEATURE_SIMILAR_MOVIES = ":featuresimilarmovies"
+    const val NAVIGATION = ":navigation"
+}
