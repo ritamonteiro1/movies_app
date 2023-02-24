dc1b7a470a9e5358aff212082c794252e3496d66
ritamonteiro1 <ritaalvesmonteiro@gmail.com>
2023-02-24 17:18:04 -0300

    create coreui module

---
+++ coreui/build.gradle.kts
@@ -0,0 +1,44 @@
+plugins {
+    id(BuildPlugins.ANDROID_LIBRARY)
+    id(BuildPlugins.KOTLIN_ANDROID)
+}
+
+apply {
+    from("../config/detekt/detekt.gradle")
+}
+
+android {
+    compileSdk = Android.COMPILE
+    buildToolsVersion = AppConfig.BUILD_TOOLS_VERSION
+
+    defaultConfig {
+        minSdk = Android.MIN
+        targetSdk = Android.TARGET
+        testInstrumentationRunner = Android.TEST_INSTRUMENTATION_RUNNER
+    }
+
+    buildTypes {
+        getByName("release") {
+            isMinifyEnabled = false
+            proguardFiles(
+                getDefaultProguardFile("proguard-android-optimize.txt"), "proguard-rules.pro"
+            )
+        }
+    }
+
+    compileOptions {
+        sourceCompatibility = AndroidCompileOptions.SOURCE_COMPATIBILITY
+        targetCompatibility = AndroidCompileOptions.TARGET_COMPATIBILITY
+    }
+
+    tasks.withType<org.jetbrains.kotlin.gradle.tasks.KotlinCompile> {
+        kotlinOptions {
+            jvmTarget = Android.JVM_TARGET
+        }
+    }
+}
+
+dependencies {
+    androidx()
+    coroutines()
+}
