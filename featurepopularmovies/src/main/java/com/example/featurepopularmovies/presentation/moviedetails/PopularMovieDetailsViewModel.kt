24e168cff7a973a503314772835651142f129154
Álvaro Prado <56163128+alvarobcprado@users.noreply.github.com>
2023-02-25 15:52:19 -0400

    feat: update PopularMovieDetailsViewModel to implements BaseViewModel

--- featurepopularmovies/src/main/java/com/example/featurepopularmovies/presentation/moviedetails/PopularMovieDetailsViewModel.kt
+++ featurepopularmovies/src/main/java/com/example/featurepopularmovies/presentation/moviedetails/PopularMovieDetailsViewModel.kt
@@ -1,29 +1,26 @@
 package com.example.featurepopularmovies.presentation.moviedetails
 
-import androidx.lifecycle.ViewModel
 import androidx.lifecycle.viewModelScope
+import com.example.core.utils.Result
+import com.example.coreui.viewmodel.BaseViewModel
+import com.example.coreui.viewmodel.EffectUi
+import com.example.coreui.viewmodel.EventUi
+import com.example.coreui.viewmodel.StateUi
 import com.example.featurepopularmovies.domain.models.PopularMovieDetail
 import com.example.featurepopularmovies.domain.repository.PopularMoviesRepository
 import kotlinx.coroutines.Dispatchers
 import kotlinx.coroutines.launch
 import kotlin.coroutines.CoroutineContext
-import com.example.core.utils.Result
-import kotlinx.coroutines.flow.*
 
 class PopularMovieDetailsViewModel(
     private val repository: PopularMoviesRepository,
     private val dispatcher: CoroutineContext = Dispatchers.Main
-) : ViewModel() {
-    private val _state =
-        MutableStateFlow<PopularMovieDetailsContract.State>(PopularMovieDetailsContract.State.Loading)
-    val state: StateFlow<PopularMovieDetailsContract.State> = _state
+) : BaseViewModel<PopularMovieDetailsContract.Event, PopularMovieDetailsContract.State, PopularMovieDetailsContract.Effect>() {
+    override fun createInitialState(): PopularMovieDetailsContract.State {
+        return PopularMovieDetailsContract.State.Loading
+    }
 
-    private val _effect =
-        MutableSharedFlow<PopularMovieDetailsContract.Effect>()
-    val effect: SharedFlow<PopularMovieDetailsContract.Effect> = _effect
-
-    private val _event = MutableSharedFlow<PopularMovieDetailsContract.Event>()
-    val collect = _event.onEach { event ->
+    override fun handleEvent(event: PopularMovieDetailsContract.Event) {
         when (event) {
             is PopularMovieDetailsContract.Event.GetMovieDetails -> {
                 getMovieDetails(event.movieId)
@@ -37,19 +34,7 @@ class PopularMovieDetailsViewModel(
         }
     }
 
-    private suspend fun setEffect(effect: PopularMovieDetailsContract.Effect) {
-        _effect.emit(effect)
-    }
-
-    suspend fun processEvent(event: PopularMovieDetailsContract.Event) {
-        _event.emit(event)
-    }
-
-    private fun setState(state: PopularMovieDetailsContract.State) {
-        _state.value = state
-    }
-
-    private suspend fun getMovieDetails(movieId: Int) {
+    private fun getMovieDetails(movieId: Int) {
         setState(PopularMovieDetailsContract.State.Loading)
         viewModelScope.launch(dispatcher) {
             when (val result = repository.getPopularMovieDetails(movieId)) {
@@ -63,34 +48,34 @@ class PopularMovieDetailsViewModel(
         }
     }
 
-    private suspend fun saveFavoriteMovie(movie: PopularMovieDetail) {
+    private fun saveFavoriteMovie(movie: PopularMovieDetail) {
         viewModelScope.launch(dispatcher) {
             repository.saveFavoriteMovie(movie)
-            setEffect(PopularMovieDetailsContract.Effect.SaveFavoriteMovie)
+            setEffect(PopularMovieDetailsContract.Effect.FavoriteMovieSaved)
         }
     }
 
-    private suspend fun deleteFavoriteMovie(movieId: Int) {
+    private fun deleteFavoriteMovie(movieId: Int) {
         viewModelScope.launch(dispatcher) {
             repository.deleteFavoriteMovie(movieId)
-            setEffect(PopularMovieDetailsContract.Effect.DeleteFavoriteMovie)
+            setEffect(PopularMovieDetailsContract.Effect.FavoriteMovieDeleted)
         }
     }
 }
 
 object PopularMovieDetailsContract {
-    sealed interface Event {
+    sealed interface Event : EventUi {
         data class GetMovieDetails(val movieId: Int) : Event
         data class SaveFavoriteMovie(val movie: PopularMovieDetail) : Event
         data class DeleteFavoriteMovie(val movieId: Int) : Event
     }
 
-    sealed interface Effect {
-        object DeleteFavoriteMovie : Effect
-        object SaveFavoriteMovie : Effect
+    sealed interface Effect : EffectUi {
+        object FavoriteMovieSaved : Effect
+        object FavoriteMovieDeleted : Effect
     }
 
-    sealed interface State {
+    sealed interface State : StateUi {
         object Loading : State
         data class Success(val movie: PopularMovieDetail) : State
         data class Error(val error: Throwable) : State
