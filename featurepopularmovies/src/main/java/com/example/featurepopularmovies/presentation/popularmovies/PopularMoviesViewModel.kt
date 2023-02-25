352fe868929aacab647b2d78254e96f180ba6a02
Álvaro Prado <56163128+alvarobcprado@users.noreply.github.com>
2023-02-25 15:56:44 -0400

    feat: update PopularMoviesViewModel to implements BaseViewModel

--- featurepopularmovies/src/main/java/com/example/featurepopularmovies/presentation/popularmovies/PopularMoviesViewModel.kt
+++ featurepopularmovies/src/main/java/com/example/featurepopularmovies/presentation/popularmovies/PopularMoviesViewModel.kt
@@ -1,28 +1,46 @@
 package com.example.featurepopularmovies.presentation.popularmovies
 
-import androidx.lifecycle.ViewModel
 import androidx.lifecycle.viewModelScope
 import androidx.paging.PagingData
 import androidx.paging.cachedIn
+import com.example.coreui.viewmodel.BaseViewModel
+import com.example.coreui.viewmodel.EffectUi
+import com.example.coreui.viewmodel.EventUi
+import com.example.coreui.viewmodel.StateUi
 import com.example.featurepopularmovies.domain.models.PopularMovie
 import com.example.featurepopularmovies.domain.repository.PopularMoviesRepository
 import kotlinx.coroutines.flow.Flow
 
 class PopularMoviesViewModel(
     private val repository: PopularMoviesRepository,
-) : ViewModel() {
+) : BaseViewModel<PopularMoviesContract.Event, PopularMoviesContract.State, PopularMoviesContract.Effect>() {
 
-    fun getPopularMovies(): Flow<PagingData<PopularMovie>> {
+    override fun createInitialState(): PopularMoviesContract.State {
+        return PopularMoviesContract.State.Initial
+    }
+
+    override fun handleEvent(event: PopularMoviesContract.Event) {
+        when (event) {
+            is PopularMoviesContract.Event.GetPopularMovies -> {
+                getPopularMovies()
+            }
+        }
+    }
+
+    private fun getPopularMovies(): Flow<PagingData<PopularMovie>> {
         return repository.getPopularMovies().cachedIn(viewModelScope)
     }
+
 }
 
 object PopularMoviesContract {
-    sealed interface Events {
-        object GetPopularMovies : Events
+    sealed interface Event : EventUi {
+        object GetPopularMovies : Event
     }
 
-    sealed interface Effects {
-        object ShowPopularMovies : Effects
+    sealed interface Effect : EffectUi
+
+    sealed interface State : StateUi {
+        object Initial : State
     }
 }
