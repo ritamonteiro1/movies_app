dc2af8c48dde289e961d3e87db88c216fd50daee
ritamonteiro1 <ritaalvesmonteiro@gmail.com>
2023-02-27 21:54:44 -0300

    inject view models

--- di/src/main/java/com/example/di/FeatureSimilarMoviesModule.kt
+++ di/src/main/java/com/example/di/FeatureSimilarMoviesModule.kt
@@ -1,7 +1,9 @@
 package com.example.di
 
+import com.example.featuresimilarmovies.presentation.similarmovies.SimilarMoviesViewModel
+import org.koin.androidx.viewmodel.dsl.viewModel
 import org.koin.dsl.module
 
 val featureSimilarMoviesModule = module {
-
+    viewModel { SimilarMoviesViewModel(get()) }
 }
--- di/src/main/java/com/example/di/NavigationModule.kt
+++
@@ -1,7 +0,0 @@
-package com.example.di
-
-import org.koin.dsl.module
-
-val navigationModule = module {
-
-}
