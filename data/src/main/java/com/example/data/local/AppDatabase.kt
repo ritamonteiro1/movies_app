81853e317cdb1a9ea4aba08f1ed0b5a92f4e0c20
ritamonteiro1 <ritaalvesmonteiro@gmail.com>
2023-03-01 17:29:52 -0300

    change in AppDatabase

--- data/src/main/java/com/example/data/local/AppDatabase.kt
+++ data/src/main/java/com/example/data/local/AppDatabase.kt
@@ -2,9 +2,10 @@ package com.example.data.local
 
 import androidx.room.Database
 import androidx.room.RoomDatabase
+import com.example.data.local.datasource.PopularMoviesCacheDataSource
 import com.example.data.local.models.MovieDetailCache
 
 @Database(entities = [MovieDetailCache::class], version = 1)
 abstract class AppDataBase : RoomDatabase() {
-    abstract fun movieDetailsCache(): MovieDetailCache
+    abstract fun movieDetailsCache(): PopularMoviesCacheDataSource
 }
